074e34c8d37af3f8aa314b484745052f624c1a37
Bruno Fabre <bruno.hfabre@gmail.com>
2023-07-19 14:43:51 -0300

    add menu

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,17 +1,62 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import { List } from '@phosphor-icons/react'
+
+import { resources } from '../../resources'
+import { Badge } from '../Badge'
 import { Search } from './Search'
 
 export function PageHeader() {
+  const [open, setOpen] = useState(false)
+
   return (
-    <div className="flex h-16 border-b border-zinc-200 items-center justify-between">
-      <Link className="flex items-center font-semibold w-64 h-16 p-4" to="/">
-        Orbit
-      </Link>
+    <>
+      {open && (
+        <div className="fixed top-16 right-0 bottom-0 left-0 p-4 flex flex-col bg-white z-10 md:hidden">
+          {resources.map((resource) => (
+            <div key={resource.name} className="flex flex-col gap-2">
+              <span className="text-base font-medium">{resource.name}</span>
 
-      <Search />
+              <div className="flex flex-col gap-2">
+                {resource.routes.map((route) => (
+                  <Link
+                    key={route.name}
+                    className="flex items-center cursor-pointer text-zinc-500 hover:text-zinc-900"
+                    to={`/${resource.name
+                      .toLowerCase()
+                      .replace(' ', '-')}/${route.name
+                      .toLowerCase()
+                      .replace(' ', '-')}`}
+                    onClick={() => setOpen(false)}
+                  >
+                    <div className="w-12 flex">
+                      <Badge method={route.method} />
+                    </div>
 
-      <div className="w-64 hidden md:flex" />
-    </div>
+                    <span className="text-sm">{route.name}</span>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+
+      <div className="flex h-16 border-b border-zinc-200 items-center justify-between">
+        <Link className="flex items-center font-semibold w-64 h-16 p-4" to="/">
+          Orbit
+        </Link>
+
+        <Search />
+
+        <button
+          className="h-16 w-16 flex items-center justify-center md:flex md:w-64"
+          onClick={() => setOpen((prevState) => !prevState)}
+        >
+          <List size={20} weight="bold" className="md:hidden" />
+        </button>
+      </div>
+    </>
   )
 }
