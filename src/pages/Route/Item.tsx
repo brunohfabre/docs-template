aa0f7f3a37986a824fddf352133faebaec6495ab
Bruno Fabre <bruno.hfabre@gmail.com>
2023-07-19 14:33:23 -0300

    add required red

--- src/pages/Route/Item.tsx
+++ src/pages/Route/Item.tsx
@@ -7,9 +7,12 @@ interface ItemProps {
 export function Item({ item }: ItemProps) {
   return (
     <div className="flex flex-col py-2">
-      <div className="flex gap-3">
+      <div className="flex gap-2 items-baseline">
         <span className="font-medium text-zinc-900">{item.name}</span>
-        {item.required && <span className="text-zinc-500">required</span>}
+
+        {item.required && (
+          <span className="text-sm text-red-500">required</span>
+        )}
       </div>
 
       <div className="text-sm text-zinc-500">
